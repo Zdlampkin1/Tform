c00a9d43d937435a171211b93aaa524a3e3e5abd
Zdlampkin1 <zdlampkin@gmail.com>
2025-04-27 20:09:05 -0500

    changed variables and createinstance files

--- Terraform Variables Detailed/createinstance.tf
+++ Terraform Variables Detailed/createinstance.tf
@@ -1,8 +1,10 @@
 resource "aws_instance" "MyFirstInstnace" {
-  ami           = "ami-0b0ea68c435eb488d"
+  ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
 
   tags = {
     Name = "demoinstnce"
   }
+
+ security_groups = var.Security_Group
 }
