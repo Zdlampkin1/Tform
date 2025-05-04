a884c1291349672781c43e0c734af1cd86ede8dd
Zdlampkin1 <zdlampkin@gmail.com>
2025-05-03 20:33:18 -0500

    up s3 bucket name

--- Terraform for AWS Cloud/AWS IAM-Roles/s3bucket.tf
+++ Terraform for AWS Cloud/AWS IAM-Roles/s3bucket.tf
@@ -1,10 +1,10 @@
 #Create AWS S3 Bucket
 
 resource "aws_s3_bucket" "levelup-s3bucket" {
-  bucket = "levelup-bucket-141"
+  bucket = "levelup-bucket-999"
   acl    = "private"
 
   tags = {
-    Name = "levelup-bucket-141"
+    Name = "levelup-bucket-999"
   }
 }
